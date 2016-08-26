3199ba50c41ae35942f0466462dc5fb0b283ee5d
Brandon Gamber <brandonwgamber@gmail.com>
2016-08-26 08:59:12 +0000

    Added path to create user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,6 @@
 class ProfilesController < ApplicationController
-    
+    def new
+       # Form where a user can fill out their own profile
+       @user = User.find( params[:user_id] )
+    end    
 end    
